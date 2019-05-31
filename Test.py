012eb0dd92ce7fdcc7deab1c9f1c3301e97359bd
218949 <218949@student.pwr.edu.pl>
2019-05-31 16:20:45 +0200

    Test add

--- Test.py
+++ Test.py
@@ -1,7 +1,14 @@
-lista = []
-lista.append()
+from __future__ import print_function
+import wave
 
 
+sample = wave.open("Classic_128.wav")
+parametry = sample.getparams()
+print("Parametry pliku oryginalnego:   ", parametry)
+
+sample_deg = wave.open("Classic_8.wav")
+parametry_deg = sample.getparams()
+print("Parametry pliku oryginalnego:   ", parametry_deg)
 
 
 
